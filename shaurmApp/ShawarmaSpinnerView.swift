9da217a244661e60d8c9f744829b8fb28dfb9b10
tmac-balla <avanteggarde@gmail.com>
2016-03-12 21:10:51 -0500

    added "no connection" warning label

--- shaurmApp/ShawarmaSpinnerView.swift
+++ shaurmApp/ShawarmaSpinnerView.swift
@@ -12,16 +12,20 @@ class ShawarmaSpinnerView: UIView {
     
     var spinnerImageView:UIImageView!
     var rotating = Bool()
+    var spinnerTimer = NSTimer()
     
     override init(frame: CGRect) {
         let screenSize: CGRect = UIScreen.mainScreen().bounds
-        super.init(frame: CGRect(x: screenSize.size.width/2 - 25, y: screenSize.size.height/2 - 25, width: 50, height: 50))
+        super.init(frame: CGRect(x: screenSize.size.width/2 - 25, y: screenSize.size.height/2 - 15, width: 50, height: 50))
         spinnerImageView = UIImageView(frame: CGRect(x: 5, y: 5, width: 40, height: 40))
         spinnerImageView.image = UIImage(named: "shaurma")
         
         self.backgroundColor = UIColor.whiteColor()
         self.layer.cornerRadius = self.frame.size.width/2
         self.addSubview(spinnerImageView)
+        
+        spinnerTimer = NSTimer(timeInterval: 15.0, target: self, selector: "throwWarning", userInfo: nil, repeats: false)
+        NSRunLoop.currentRunLoop().addTimer(spinnerTimer, forMode: NSRunLoopCommonModes)
         
     }
 
@@ -32,6 +36,29 @@ class ShawarmaSpinnerView: UIView {
     func start() {
         rotating = true
         rotate()
+    }
+    
+    func throwWarning(){
+        
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        self.frame = CGRect(x: screenSize.size.width/2 - 60, y: screenSize.size.height/2 - 65, width: 120, height: 80)
+        self.layer.cornerRadius = self.frame.size.width/10
+
+        let warningLabel = UILabel(frame: CGRect(x: self.frame.width/2 - 50, y: self.frame.height - 35, width: 100, height: 40))
+        warningLabel.font = UIFont(name: "Montserrat-Light", size: 14)
+        warningLabel.text = "нет соединения"
+        warningLabel.sizeToFit()
+        
+        
+        let crossImage = UIImageView(frame: CGRect(x: self.frame.width/2 - 18, y: 5, width: 36, height: 36))
+        crossImage.image = UIImage(named: "cross")
+        
+        self.addSubview(warningLabel)
+        self.addSubview(crossImage)
+
+
+        self.spinnerImageView.removeFromSuperview()
+        
     }
     
     func rotate() {
