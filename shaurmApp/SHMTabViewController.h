cc0ede1700ebd1cdf5a4590ad84955b1d6076fa8
Игорь Николаев <nikolaevigor95@mail.ru>
2016-03-14 17:45:26 +0300

    tutorial on first launch added

---
+++ shaurmApp/SHMTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHMTabViewController.h
+//  shaurmApp
+//
+//  Created by Igor Nikolaev on 14/03/16.
+//  Copyright © 2016 NikolaevIgor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHMTabViewController : UITabBarController
+
+@end
---
+++ shaurmApp/SHMViewController.m
@@ -0,0 +1,82 @@
+//
+//  SHMViewController.m
+//  shaurmApp
+//
+//  Created by Igor Nikolaev on 14/03/16.
+//  Copyright © 2016 NikolaevIgor. All rights reserved.
+//
+
+#import "SHMViewController.h"
+#import "SHMChildViewController.h"
+
+@interface SHMViewController ()
+
+@end
+
+@implementation SHMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.pageController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    
+    self.pageController.dataSource = self;
+    [[self.pageController view] setFrame:[[self view] bounds]];
+    
+    SHMChildViewController *initialViewController = [self viewControllerAtIndex:0];
+    
+    NSArray *viewControllers = [NSArray arrayWithObject:initialViewController];
+    
+    [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    [self addChildViewController:self.pageController];
+    [[self view] addSubview:[self.pageController view]];
+    [self.pageController didMoveToParentViewController:self];
+    
+}
+
+- (SHMChildViewController *)viewControllerAtIndex:(NSUInteger)index {
+    
+    SHMChildViewController *childViewController = [[SHMChildViewController alloc] initWithNibName:@"SHMChildViewController" bundle:nil];
+    childViewController.index = index;
+    childViewController.delegate = self;
+    
+    return childViewController;
+    
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    
+    NSUInteger index = [(SHMChildViewController *)viewController index];
+    
+    if (index == 0) {
+        return nil;
+    }
+    
+    // Decrease the index by 1 to return
+    index--;
+    
+    return [self viewControllerAtIndex:index];
+    
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    
+    NSUInteger index = [(SHMChildViewController *)viewController index];
+    
+    index++;
+    
+    if (index == 5) {
+        return nil;
+    }
+    
+    return [self viewControllerAtIndex:index];
+    
+}
+
+- (void)finishTutorial
+{
+    [self performSegueWithIdentifier:@"finishTutorial" sender:nil];
+}
+
+@end
