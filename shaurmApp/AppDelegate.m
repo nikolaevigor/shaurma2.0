a7398576a411db1bbe68375df78ef1b8f9226d5c
tmac-balla <avanteggarde@gmail.com>
2016-03-15 18:56:23 -0400

    added pic

--- shaurmApp/AppDelegate.m
+++ shaurmApp/AppDelegate.m
@@ -18,6 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
